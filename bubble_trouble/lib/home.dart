7815950ecad6eb615a871232cc780b651918c67f
ChitTI <chetanjagdaley11@gmail.com>
2021-10-29 13:50:37 +0530

    added a ball which can move around and a missile which is working fine

--- bubble_trouble/lib/home.dart
+++ bubble_trouble/lib/home.dart
@@ -1,5 +1,7 @@
 import 'dart:async';
 
+import 'package:bubble_trouble/ball.dart';
+import 'package:bubble_trouble/missile.dart';
 import 'package:bubble_trouble/player.dart';
 import 'package:flutter/material.dart';
 import 'button.dart';
@@ -11,14 +13,21 @@ class Homepage extends StatefulWidget {
   _HomepageState createState() => _HomepageState();
 }
 
+enum direction { LEFT, RIGHT }
+
 class _HomepageState extends State<Homepage> {
   // player variables
   static double playerX = 0;
 
   //missile variables
   double missileX = playerX;
-  double missileY = 1;
   double missileHeight = 10;
+  bool midshot = false;
+
+  //ball variables
+  double ballX = 0.5;
+  double ballY = 0;
+  var ballDirection = direction.LEFT;
 
   void resetMissile() {
     missileX = playerX;
@@ -27,37 +36,65 @@ class _HomepageState extends State<Homepage> {
 
   void moveLeft() {
     setState(() {
-     
       if (playerX - 0.1 < -1) {
         //do nothing
       } else {
         playerX -= .1;
       }
-       missileX = playerX;
+      if (!midshot) {
+        missileX = playerX;
+      }
     });
   }
 
   void moveRight() {
     setState(() {
-      
       if (playerX + .1 > 1) {
         //do nothing
       } else {
         playerX += .1;
       }
-       missileX = playerX;
+      if (!midshot) {
+        missileX = playerX;
+      }
     });
-   
   }
 
   void fireMissile() {
-    Timer.periodic(Duration(milliseconds: 30), (timer) {
-      if (missileHeight > MediaQuery.of(context).size.height * 3 / 4) {
-        resetMissile();
-        timer.cancel();
-      } else {
+    if (midshot == false) {
+      Timer.periodic(Duration(milliseconds: 30), (timer) {
+        //shot fired and we dont want another shot right away
+        midshot = true;
+
+        // missile will go up until it reaches the top
         setState(() {
           missileHeight += 10;
+        });
+
+        if (missileHeight > MediaQuery.of(context).size.height * 3 / 4) {
+          resetMissile();
+          timer.cancel();
+          midshot = false;
+        }
+      });
+    }
+  }
+
+  void startGame() {
+    Timer.periodic(Duration(milliseconds: 50), (Timer) {
+      if (ballX - 0.03 < -1) {
+        ballDirection = direction.RIGHT;
+      } else if (ballX + 0.03 > 1) {
+        ballDirection = direction.LEFT;
+      }
+
+      if (ballDirection == direction.LEFT) {
+        setState(() {
+          ballX -= .03;
+        });
+      } else if (ballDirection == direction.RIGHT) {
+        setState(() {
+          ballX += .03;
         });
       }
     });
@@ -74,13 +111,10 @@ class _HomepageState extends State<Homepage> {
               child: Center(
                 child: Stack(
                   children: [
-                    Container(
-                      alignment: Alignment(missileX, missileY),
-                      child: Container(
-                        width: 5,
-                        height: missileHeight,
-                        color: Colors.grey,
-                      ),
+                    MyBall(ballX: ballX, ballY: ballY),
+                    MyMissile(
+                      missileHeight: missileHeight,
+                      missileX: missileX,
                     ),
                     MyPlayer(
                       playerX: playerX,
@@ -95,6 +129,10 @@ class _HomepageState extends State<Homepage> {
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
+              MyButton(
+                icon: Icons.play_arrow,
+                function: startGame,
+              ),
               MyButton(
                 icon: Icons.arrow_back,
                 function: moveLeft,
